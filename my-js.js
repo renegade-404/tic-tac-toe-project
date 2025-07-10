a81ba9caefc568e3904a1455641c9e03cc87a25a
renegade-404 <mypcisrenegade@gmail.com>
2025-07-10 17:52:38 +0900

    add random position choice function

--- my-js.js
+++ my-js.js
@@ -13,14 +13,25 @@ function createComputer() {
     const computerMark = "X";
 
     function choosePosition(arr) {
-        const indexesList = arr.map((item, index) => {
-            if (item == "") return index;
-        });
-        const freePos = indexesList.filter(item => item !== undefined);
-        let randomNum = Math.floor(Math.random() * freePos.length);
+  
+        function drawIndex(arr) {
+            return arr
+                .map((item, index) => {
+                    if (Array.isArray(item)) {
+                        if (item.includes("")) return index
+                    } else if (item === "") return index
+                })
+                .filter(item => item !== undefined);
+        }
 
-        return freePos[randomNum];
-        
+        const mainArr = drawIndex(arr);
+        const randMainIndex = Math.floor(Math.random() * mainArr.length);
+
+        const subArr = drawIndex(arr[mainArr[randMainIndex]]);
+        const randSubIndex = Math.floor(Math.random() * subArr.length);
+
+        return [mainArr[randMainIndex], subArr[randSubIndex]];
+
     }
 
     return {computerMark, choosePosition}
