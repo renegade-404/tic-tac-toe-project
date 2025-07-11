0fa5a5b742e11739ec0577db0992dd9a2769b452
renegade-404 <mypcisrenegade@gmail.com>
2025-07-11 18:32:00 +0900

    add player position check function

--- my-js.js
+++ my-js.js
@@ -1,3 +1,37 @@
+const Board = (function(){
+    let board = [["", "", ""], ["", "", ""], ["", "", ""]];
+    const winningPositions = {
+        1: [[0, 0,], [0, 1], [0, 2]],
+        2: [1, [0, 1, 2]],
+        3: [2, [0, 1, 2]],
+    };
+
+    function checkPos(playerMark) { // returns an array of  current positions
+        const column = drawNestIndex(board, playerMark);
+        let currentPos = [];
+
+        if (column.length > 0) {
+            for (let i = 0; i < column.length; i++) {
+                let squares = drawIndex(board[column[i]], playerMark);
+                let sortedSquares = squares.map(item => [column[i], item]);
+                currentPos.push(sortedSquares);
+            }
+        }
+        return currentPos;
+    }
+
+    const setPosition = function(position, mark) {
+        board[position[0]].splice(position[1], 1, mark);
+    }
+
+    const checkForWin = function() {
+
+    }
+
+    return {setPosition, board}
+})();
+
+
 function createPlayer(name) {
     const playerName = name;
     const playerMark = "O";
@@ -12,7 +46,7 @@ function createPlayer(name) {
 function createComputer() {
     const computerMark = "X";
 
-    function choosePosition(arr) {
+    function choosePosition(arr) { // return an array of open position indexes
   
         function drawIndex(arr) {
             return arr
